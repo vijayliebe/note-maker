a5b62658b6634e4012bf46b8a631cb78cf09c512
vijayliebe <flyhighvijay@gmail.com>
2022-01-25 02:59:14 +0530

    filter and sorting

--- src/app/home/components/notes-list/notes-list.component.ts
+++ src/app/home/components/notes-list/notes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
 import { DatacontextService } from "src/app/data/datacontext.service";
 import { AddNotesComponent } from '../add-notes/add-notes.component';
 import { ConfirmDialogComponent } from '../../../shared/components/confirm-dialog/confirm-dialog.component';
@@ -16,6 +17,14 @@ import { CommonNotificationService } from "src/app/shared/services/common-notifi
   styleUrls: ['./notes-list.component.scss']
 })
 export class NotesListComponent implements OnInit {
+  originalData: any;
+  sorting: any = [{"name": "Importance", "key": "imp"}, {"name": "Difficulty", "key": "diff"}];
+  levels: any = [1,2,3,4,5];
+  selectedSort = new FormControl("");
+  selectedTags = new FormControl();
+  selectedImp = new FormControl();
+  selectedDiff = new FormControl();
+  uniqueTags: any = [];
   addNoteFlag : boolean = false;
   @Input() subjectName: any;
   notes: any = [];
@@ -32,27 +41,26 @@ export class NotesListComponent implements OnInit {
     this.datacontextService.notesService.getNote(this.subjectName).subscribe((res)=>{
       console.log("getNote ::", res);
       this.notes = res;
+      this.prepareUniqueTags(this.notes || []);
     }); 
+  }
+
+  prepareUniqueTags(noteList){
+    let tagMap = {};
+    for(let i=0; i < noteList.length; i++){
+        let tags = noteList[i]['tags'] || [];
+        
+        for(let j=0; j< tags.length; j++){
+          let tag = tags[j]['name'];
+          if(!(tag in tagMap)) tagMap[tag] = 1;
+        }
+    }
+    this.uniqueTags = Object.keys(tagMap);
+    console.log("this.uniqueTags ::", this.uniqueTags);
   }
 
   addNote(id?){
     this.addNoteFlag = true;
-    // const dialogRef = this.dialog.open(AddNotesComponent, {
-    //   panelClass: 'full-screen-dialog-container',
-    //   data: {"id": id}
-    // });
-
-    // dialogRef.afterClosed().subscribe((result) => {
-    //   if(result){
-    //     this.getNoteList();
-    //     if(result && result.id){
-    //       this.commonNotificationService.showSnackBar(" Note is edited successfully", 'success', 2000);
-    //     } else {
-    //       this.commonNotificationService.showSnackBar(" Note is added successfully", 'success', 2000);
-    //     }
-    //   }
-    // });
-
   }
 
   editNote(noteData, idx){
@@ -95,6 +103,22 @@ export class NotesListComponent implements OnInit {
         });
       }
     });
+  }
+
+  onTagSelect(e){
+    console.log("onTagSelect :: selectedTags ::", this.selectedTags.value);
+  }
+
+  onImpSelect(e){
+    console.log("onImpSelect :: selectedImp ::", this.selectedImp.value);
+  }
+
+  onDiffSelect(e){
+    console.log("onDiffSelect :: selectedDiff ::", this.selectedDiff.value);
+  }
+
+  onSortSelect(e){
+    console.log("onSortSelect :: selectedSort ::", this.selectedSort.value);
   }
 
 }
